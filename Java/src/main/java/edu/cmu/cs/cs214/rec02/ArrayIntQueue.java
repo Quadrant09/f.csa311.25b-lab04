da85b822e90eedb03b9b91af2b2e715499ce3649
Quadrant09 <amarboldenkhbold9@gmail.com>
2025-03-14 16:23:15 +0800

    first11

--- Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
@@ -7,51 +7,33 @@ import java.util.Arrays;
  * the queue starts out at the head of the array, allowing the queue to grow and
  * shrink in constant time.
  *
- * TODO: This implementation contains three bugs! Use your tests to determine the
- * source of the bugs and correct them!
+ * Fixed bugs:
+ * 1. `isEmpty` logic was incorrect.
+ * 2. `peek` could throw an exception on an empty queue.
+ * 3. `ensureCapacity` had a faulty element-copying loop.
  *
  * @author Alex Lockwood
  * @author Ye Lu
  */
 public class ArrayIntQueue implements IntQueue {
 
-    /**
-     * An array holding this queue's data
-     */
     private int[] elementData;
-
-    /**
-     * Index of the next dequeue-able value
-     */
     private int head;
-
-    /**
-     * Current size of queue
-     */
     private int size;
-
-    /**
-     * The initial size for new instances of ArrayQueue
-     */
     private static final int INITIAL_SIZE = 10;
 
-    /**
-     * Constructs an empty queue with an initial capacity of ten.
-     */
     public ArrayIntQueue() {
         elementData = new int[INITIAL_SIZE];
         head = 0;
         size = 0;
     }
 
-    /** {@inheritDoc} */
     public void clear() {
         Arrays.fill(elementData, 0);
         size = 0;
         head = 0;
     }
 
-    /** {@inheritDoc} */
     public Integer dequeue() {
         if (isEmpty()) {
             return null;
@@ -62,7 +44,6 @@ public class ArrayIntQueue implements IntQueue {
         return value;
     }
 
-    /** {@inheritDoc} */
     public boolean enqueue(Integer value) {
         ensureCapacity();
         int tail = (head + size) % elementData.length;
@@ -71,35 +52,28 @@ public class ArrayIntQueue implements IntQueue {
         return true;
     }
 
-    /** {@inheritDoc} */
     public boolean isEmpty() {
-        return size >= 0;
+        return size == 0;
     }
 
-    /** {@inheritDoc} */
     public Integer peek() {
+        if (isEmpty()) {
+            return null;
+        }
         return elementData[head];
     }
 
-    /** {@inheritDoc} */
     public int size() {
         return size;
     }
 
-    /**
-     * Increases the capacity of this <tt>ArrayIntQueue</tt> instance, if
-     * necessary, to ensure that it can hold at least size + 1 elements.
-     */
     private void ensureCapacity() {
         if (size == elementData.length) {
             int oldCapacity = elementData.length;
             int newCapacity = 2 * oldCapacity + 1;
             int[] newData = new int[newCapacity];
-            for (int i = head; i < oldCapacity; i++) {
-                newData[i - head] = elementData[i];
-            }
-            for (int i = 0; i < head; i++) {
-                newData[head - i] = elementData[i];
+            for (int i = 0; i < size; i++) {
+                newData[i] = elementData[(head + i) % oldCapacity];
             }
             elementData = newData;
             head = 0;
